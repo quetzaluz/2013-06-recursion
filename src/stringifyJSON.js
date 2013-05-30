716ccdc737ca894428701b195c948976d379345e
quetzaluz <quetzaluz@gmail.com>
2013-05-29 22:31:28 -0700

    Number, boolean, and null objs stringify.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,21 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
-};
+  //
+  // I know this code is bad, but I am trying to pass tests
+  // in the spec one at a time. Plan to add if statements to
+  // determine argument types and stringify appropriately.
+  var stringified;
+  if (typeof obj === 'number') {
+    stringified = obj.toString();
+  }
+  if (typeof obj === 'boolean' || 'null') {
+    stringified = String(obj);
+  }
+  else {
+    for (var i = 0; i < obj.length; i++) {
+      stringified += obj[i]
+    }
+  }
+  return stringified;
+}
