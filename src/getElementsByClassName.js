007b8a1ff3da48aa78867eb6c02d7a9df43d348c
Cyd Quetzal La Luz <cqlaluz@students.transcodesf.org>
2013-06-03 15:44:40 -0700

    getElementsByClassName returns correct list, but does not pass spec.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,34 @@
 // };
 
 // But in stead we're going to implement it from scratch:
+
 var getElementsByClassName = function (className) {
-  // your code here
+  var classList = [];
+  function searchForClass (node, className) {
+  	//local function to search through childNode list for classname
+  	for (var j = 0; j < node.classList['length']; j++) {
+        if (node.classList[j] === className) {
+            //console.log("Class found:" + document.body.childNodes[i].classList[j] + " IN ELEMENT:")
+            //console.log(document.body.childNodes[i])
+  	        classList.push(node);
+        }
+    }
+  }
+  function iterateThroughNodes (nodes) {
+  	  //local function to see if childNode list has classList or
+  	  //other childNode lists. Calls recursively if more nodes.
+  for (var i = 0; i < nodes.length; i++) {
+  	//iterate through child nodes, see if they have a class list
+  	  if (nodes[i].classList){
+  	  	searchForClass(nodes[i], className);
+      }
+      if (nodes[i].childNodes) {
+      	iterateThroughNodes(nodes[i].childNodes)
+      }
+    }
+  }
+  iterateThroughNodes(document.body.childNodes)
+  //console.log("ClassList:")
+  //console.log(classList);
+  return classList;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,10 +4,6 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
-  //
-  // I know this code is bad, but I am trying to pass tests
-  // in the spec one at a time. Plan to add if statements to
-  // determine argument types and stringify appropriately.
   var stringified;
   if (typeof obj === 'number') return obj.toString();
   else if (typeof obj === 'boolean') return String(obj);
